58f67b239c5ecda2c9ea4258f6b349b894d815c8
StKala <pmcb13@knights.ucf.edu>
2018-03-11 17:24:27 -0400

    More Find Mentor Modifications, added fake API and button

--- components/user/mentorItem.js
+++ components/user/mentorItem.js
@@ -2,38 +2,60 @@ import React, { Component } from 'react';
 
 
 export default class MentorItem extends Component {
+
 	constructor(props) {
 		super(props);
 	}
+	
+	fancyAlert(){
+		
+		alert('fancy click');
+		
+	}
+	
+	
 	render(props) {
+			
 		return (
 			<div className="mentor item">
 				
 				<div className="profile picture" style={{ display:'inline-block'}}>
+					{"Picture: "} 
+					{this.props.thing}
 					Profile Picture
 				</div>
 				
 				<div className="profile wrapper" style={{ display:'inline-block'}}>
 					
 					<div className="profile description" >
-						Profile Description
+						{"Bio: "} 
+						{this.props.profileText}
 					</div>
 					
 					<div className="profile tags" >
-						Tags
+						{"Tags: "} 
+						{this.props.tags.map((test) => <li > {test} </li> )}
 					</div>
 					
 					<div className="profile rating" >
-						Rating
+						{"Rating: "} 
+						{this.props.rating}
+					</div>
+					
+					<div className="distance" >
+						{"Distance: "} 
+						{this.props.distanceAway}
 					</div>
 					
 				</div>
-				
 								
 				<div className="message button" style={{ display:'inline-block'}}>
-					MENTOR MEEEEE!!!
+					<button className="square" onClick={() => this.fancyAlert()}>
+						{"MENTOR ME!"}
+					</button>
 				</div>
 				
+							
 			</div>
 		);
 	}
