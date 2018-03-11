8f9a0c58d1a3b7e562fe71bd2c8c9fef20568271
Ussama Azam <nuclearoreo@gmail.com>
2018-03-11 13:28:31 -0400

    Fixed contact page

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,7 @@ const pageTitle = "Contact";
 
 export default () => (
 	<div>
-		<Head title={pageTitle} cssFiles={ ["homepage.css"] }/>
+		<Head title={pageTitle} cssFiles={ ["contact.css"] }/>
 		<div className="app-container">
 			<div className="site-wrapper">
 				<div className="site-wrapper-inner">
