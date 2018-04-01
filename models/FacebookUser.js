6b45e0cbc8beda3c4e76c46cd8a2b6851ac7d0c1
Nathan Dabu <contactnathand@gmail.com>
2018-04-01 16:41:48 -0400

    ⛔️ removed old models folder

--- models/FacebookUser.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-
-const userSchema = new Schema({
-  facebookId: String,
-  displayName: String
-})
-
-mongoose.model('facebookUsers', userSchema)
--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-
-const userSchema = new Schema({
-  username: String,
-  passwordHash: String
-})
-
-mongoose.model('users', userSchema)
