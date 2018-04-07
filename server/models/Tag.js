21952acd3b4e9a1abd639ff66b3aa053b6efd149
Dena Alawi <denjalawi@gmail.com>
2018-04-07 13:26:51 -0400

    :white_check_mark: Tag API-> findMentors and remove supported + reformatting

--- server/models/Tag.js
+++ server/models/Tag.js
@@ -3,58 +3,68 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TagsSchema = new Schema({
-  tag: {type: String, unique: true},
-  tagMentors: {type: [String]}          // email/username
+	tag: {
+			type: String,
+			unique: true
+		},
+		tagMentors: {
+			type: [String]
+		}
 })
 
 
 
 class TagsClass {
-  static publicFields() {
-    return [
-      'tag',
-      'tagMentors'
-    ];
-  }
+	static async addMentorToTag(newTag, mentor) {
+		console.log(`------>addMentorToTag function`);
+
+		const isTag = await this.findOne({tag: newTag});
+		if(!isTag){
+			const newRecord = await this.create({tag: newTag, tagMentors: mentor} );
+			console.log(`------> new "${newTag}" tag has been created`);
+			return newRecord;
+		}
+		else{
+			const addmentor = await this.findOneAndUpdate(
+						{tag: newTag},
+						{$addToSet:{tagMentors: mentor}},
+						{new: true}
+			);
+			console.log(`------->${mentor} is added to the "${newTag}" tag`);
+			return addmentor;
+		}
+	};
+
+	static async exploreAllTags() {
+		const alltags = await this.find({}, {tag: 1});
+		return alltags;
+	};
+
+// note: the only reason findOne is just is to be able to return false
+	static async findMentors(lookupTag) {
+		const mentors = await this.findOne({tag: lookupTag}, {tagMentors: 1});
+		if(!mentors){return false;}
+		return mentors;
+	};
+
+// removes mentor and also deletes tag if empty
+	static async removeMentorFromTag(mentor, dtag){
+		const updatedTag = await this.findOneAndUpdate(
+			{tag: dtag},
+	    {$pull:{tagMentors: mentor}},
+	    {new: true}
+		);
+
+		const isEmpty = await this.find( { tagMentors: { $size: 0 }});
+		if(isEmpty){
+			await this.remove({tag: dtag});
+			return false;
+		}
+		return updatedTag;
+	}
 
 
-static async addMentorToTag(newTag, mentor) {
-
-  console.log(`------>addMentorToTag function`);
-
-  const isTag = await this.findOne({tag: newTag});
-  if(!isTag){
-    await this.create({tag: newTag});
-    console.log(`------> new "${newTag}" tag has been created`);
-  }
-  //else {console.log("-----> tag exists already ");}
-
-  const addmentor = await this.findOneAndUpdate(
-        {tag: newTag},
-        {$addToSet:{tagMentors: mentor}},
-        {new: true});
-
-  console.log(`------->${mentor} is added to the "${newTag}" tag`);
-  return addmentor;
-};
-
-
-
-static async exploreAllTags() {
-  const alltags = await this.find({});
-  return alltags;
-}
-
-
-
-
-
-
-
-
-
-
-
+	
 }
 
 
