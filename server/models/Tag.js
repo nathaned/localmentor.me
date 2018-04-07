60b93cd1232b3fb6d7f7ab3b220a6c1571f15983
Dena Alawi <denjalawi@gmail.com>
2018-04-06 23:53:02 -0400

    added a addMentorToTag function

--- server/models/Tag.js
+++ server/models/Tag.js
@@ -6,3 +6,41 @@ export const TagsSchema = new Schema({
   tag: {type: String, unique: true},
   tagMentors: {type: [String]}          // email/username
 })
+
+
+
+class TagsClass {
+  static publicFields() {
+    return [
+      'tag',
+      'tagMentors'
+    ];
+  }
+
+
+static async addMentorToTag( newTag, mentor){
+  console.log(`${mentor} has added themselves to the "${tag}" tag`);
+  this.findOne({tag: newtag},
+  (err, newTag) =>
+  {
+    if(err){
+      this.create({newTag});
+      console.log(` new "${tag}"  has been created`);
+    }
+      this.findOneAndUpdate(
+        {newTag},
+        {$addToSet:{mentor}},
+        {new: true},
+        (err, tag) =>
+        {
+          if(err){ res.send(err);}
+          res.send(tag);
+      });
+      console.log(`>${mentor} is added to the "${tag}" tag`);
+  })
+}
+
+
+
+
+}
