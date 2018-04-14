78b8f85ddfb2ee784a9c4dfe64f86efa6d107990
Nathan Dabu <contactnathand@gmail.com>
2018-04-13 22:02:48 -0400

    🎨 removed css that forced everything to be white 💪🏿

--- components/public/contact.js
+++ components/public/contact.js
@@ -7,7 +7,7 @@ export default class Contact extends Component {
 				<h1 className="cover-heading">Contact us</h1>
 				<p className="lead">Cover is a one-page template for building Complicated and ugly home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
 				<p className="lead">
-					<a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+					<a href="#" className="btn btn-lg btn-primary">Learn more</a>
 				</p>
 			</div>
 		);
