ddb05c96fc36599f2a5d71fe2357d4c76928ef29
Ussama Azam <nuclearoreo@gmail.com>
2018-04-16 14:48:33 -0400

    Added jumbotron to the homepage to make the text easier to read

--- components/public/homepage.js
+++ components/public/homepage.js
@@ -4,12 +4,14 @@ export default class Homepage extends Component {
 	render() {
 		return (
 			<div className="inner cover">
-				<h1 className="cover-heading">Welcome to MentorMe.</h1>
-				<p className="lead">Register and find yourself a mentor to start learning.</p>
-				<p className="lead">
-					<a href="/register" className="btn btn-lg btn-success">Register</a>
-				</p>
-			</div>
+				<div className="jumbotron trans">
+					<h1 className="cover-heading">Welcome to MentorMe.</h1>
+					<p className="lead">Register and find yourself a mentor to start learning.</p>
+					<p className="lead">
+						<a href="/register" className="btn btn-lg btn-success">Register</a>
+						</p>
+					</div>
+				</div>
 		);
 	}
 }
