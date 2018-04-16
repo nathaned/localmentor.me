37eabebc71d3ad2ff131aa6667f450c6bab8cd7e
bmw207 <ajay.narotam@knights.ucf.edu>
2018-04-16 15:36:32 -0400

    About Us

--- components/public/contact.js
+++ components/public/contact.js
@@ -6,14 +6,38 @@ export default class Contact extends Component {
 			<div className="inner cover">
 
 			<div className="jumbotron trans">
-					<h1 className="cover-heading">About Us</h1>
+					<h1 className="cover-heading">Welcome to MentorMe.</h1>
+
+					<p className="lead"> A Fun, Easy, and Safe way to find a mentor.		</p>
+
+
 					<p className="lead">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-					labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-					laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-					voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-					non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+					We all have aspects in our life that we would like to get help with.
+
+					Whether that may be a starting a new diet that has specific requirements, or learning how to finally play that guitar.
+
+					MentorMe is the one stop shop place for you. We encourage you to please make a profile, share your interests, and reach out to Mentors around you.
+
+					MentorMe is strategically built to offer a safe environment for individuals looking to find that specific someone to help you reach your goal.
+
+					We invite you to please use responsibly, and consider Mentoring aswell.
+
+
 					</p>
+
+
+					<p className="lead">Questions, Comments, Concerns?		</p>
+
+					<p className="lead">Contact us at: </p>
+					<p className="lead">contact@localmentor.me </p>
+
+
+
+
+
+
+
 			</div>
 			</div>
 		);
