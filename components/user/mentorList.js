f82a56d21e37c6897bde206301b3311424c64166
Nathan Dabu <contactnathand@gmail.com>
2018-04-16 23:33:42 -0400

    👽 safespace secured for our snowflake users ❄️

--- components/user/mentorList.js
+++ components/user/mentorList.js
@@ -7,26 +7,38 @@ export default class MentorList extends Component {
 		super(props);
 	}
 
+	okayToShow(user, profile) {
+		if (profile.username == user) {
+			return false;
+		}
+		if (profile.blocked && profile.blocked.indexOf(user) != -1) {
+			return false;
+		}
+		return true;
+	}
+
 	renderMentors() {
 		const mentors = this.props.mentors;
 		const user = this.props.user;
 		console.log("mentors in renderMentors");
 		console.log(mentors);
 		return (
-			mentors.map( (item, i) => (item.username == user ? null :
-				<ProfileCard
-					key={item.username + i}
-					actionable={item.requestedMentees.indexOf(user) == -1}
-					bio={item.mentorBio}
-					email={item.email}
-					firstName={item.firstName}
-					lastName={item.lastName}
-					location={item.location}
-					rating={item.rating500 / 100}
-					tags={item.tags}
-					type={"search"}
-					username={item.username}
-				/>
+			mentors.map( (item, i) => (
+				this.okayToShow(user, item) ? (
+					<ProfileCard
+						key={item.username + i}
+						actionable={item.requestedMentees.indexOf(user) == -1}
+						bio={item.mentorBio}
+						email={item.email}
+						firstName={item.firstName}
+						lastName={item.lastName}
+						location={item.location}
+						rating={item.rating500 / 100}
+						tags={item.tags}
+						type={"search"}
+						username={item.username}
+					/>
+				) : null
 			))
 		);
 	}
